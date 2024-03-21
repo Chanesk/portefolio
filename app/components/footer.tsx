6b85ba013c48026dc2057ee3f804fe53da0fdf95
Chanesk <chanesk01@gmail.com>
2024-03-21 08:42:02 +0300

    feat:add mailto

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Footer(){
     return(
         <footer className="md:mt-12">
-            <p className="text-center md:text-base text-sm">Prefer to email us? Send your inquiry to: <span className="font-medium text-green">agneskabanga01@gmail.com</span></p>
+            <p className="text-center md:text-base text-sm">Prefer to email us? Send your inquiry to: <Link href='mailto:agneskabanga01@gmail.com' className="font-medium text-green">agneskabanga01@gmail.com</Link></p>
             <p className="text-center text-sm pb-4 pt-2 ">©2024 Agnès KABANGA </p>
         </footer>
     )
